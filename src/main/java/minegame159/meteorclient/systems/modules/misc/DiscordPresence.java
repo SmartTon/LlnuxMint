42edacc3f91aee5eb64bbba54fa38db30e289b79
Unzzi <62009394+Unzzi@users.noreply.github.com>
2021-05-30 15:40:36 +0300

    дискорд поменял
    кастом айди и текст при наведении

--- src/main/java/minegame159/meteorclient/systems/modules/misc/DiscordPresence.java
+++ src/main/java/minegame159/meteorclient/systems/modules/misc/DiscordPresence.java
@@ -1,20 +1,13 @@
-/*
- * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client/).
- * Copyright (c) 2021 Meteor Development.
- */
-
 package minegame159.meteorclient.systems.modules.misc;
-
-//Created by squidoodly
 
 import club.minnced.discord.rpc.DiscordEventHandlers;
 import club.minnced.discord.rpc.DiscordRPC;
 import club.minnced.discord.rpc.DiscordRichPresence;
 import meteordevelopment.orbit.EventHandler;
-import minegame159.meteorclient.events.world.TickEvent;
+import minegame159.meteorclient.events.world.TickEvent.Post;
 import minegame159.meteorclient.settings.Setting;
 import minegame159.meteorclient.settings.SettingGroup;
-import minegame159.meteorclient.settings.StringSetting;
+import minegame159.meteorclient.settings.StringSetting.Builder;
 import minegame159.meteorclient.systems.config.Config;
 import minegame159.meteorclient.systems.modules.Categories;
 import minegame159.meteorclient.systems.modules.Module;
@@ -22,109 +15,107 @@ import minegame159.meteorclient.utils.Utils;
 import minegame159.meteorclient.utils.misc.Placeholders;
 
 public class DiscordPresence extends Module {
-    private final SettingGroup sgGeneral = settings.getDefaultGroup();
-
-    private final Setting<String> DiscordText = sgGeneral.add(new StringSetting.Builder()
-            .name("DiscordText")
-            .description("DiscordText")
-            .defaultValue("Linux Mint")
-            .onChanged(booleanSetting -> updateDetails())
-            .build()
-    );
-
-    private final Setting<String> line1 = sgGeneral.add(new StringSetting.Builder()
-            .name("line-1")
-            .description("The text it displays on line 1 of the RPC.")
-            .defaultValue("Linux")
-            .onChanged(booleanSetting -> updateDetails())
-            .build()
-    );
-
-    private final Setting<String> line2 = sgGeneral.add(new StringSetting.Builder()
-            .name("line-2")
-            .description("The text it displays on line 2 of the RPC.")
-            .defaultValue("Mint")
-            .onChanged(booleanSetting -> updateDetails())
-            .build()
-    );
-
+    private final SettingGroup sgGeneral;
+    private final Setting<String> line1;
+    private final Setting<String> line2;
+    private final Setting<String> DiscordText;
+    private final Setting<String> NaturalDiscordID_nogays;
     private static final DiscordRichPresence rpc = new DiscordRichPresence();
-    private static final DiscordRPC instance = DiscordRPC.INSTANCE;
-    private SmallImage currentSmallImage;
+    private static final DiscordRPC instance;
+    private DiscordPresence.SmallImage currentSmallImage;
     private int ticks;
 
     public DiscordPresence() {
         super(Categories.Misc, "discord-presence", "Displays a RPC for you on Discord to show that you're playing Meteor Client!");
+
+        this.sgGeneral = this.settings.getDefaultGroup();
+        this.line1 = this.sgGeneral.add((new Builder()).name("line-1").description("The text it displays on line 1 of the RPC.").defaultValue("{username} || {server}").onChanged((booleanSetting) -> {
+            this.updateDetails();
+        }).build());
+        this.line2 = this.sgGeneral.add((new Builder()).name("line-2").description("The text it displays on line 2 of the RPC.").defaultValue("Meteor on Crack!").onChanged((booleanSetting) -> {
+            this.updateDetails();
+        }).build());
+        this.DiscordText = this.sgGeneral.add((new Builder()).name("Discord-mode").description("Choosing mode of discord rpc").defaultValue("Linux Mint").onChanged((booleanSetting) -> {
+            this.updateDetails();
+        }).build());
+        this.NaturalDiscordID_nogays = this.sgGeneral.add((new Builder()).name("Discord_id").description("Melon hack - 847117414847873056, Linux Mint - 847488385275002920 ").defaultValue("847488385275002920").onChanged((booleanSetting) -> {
+            this.updateDetails();
+        }).build());
     }
 
-    @Override
     public void onActivate() {
+
+
         DiscordEventHandlers handlers = new DiscordEventHandlers();
-        instance.Discord_Initialize("847488385275002920", handlers, true, null);
-
+        instance.Discord_Initialize(NaturalDiscordID_nogays.get(), handlers, true, (String) null);
         rpc.startTimestamp = System.currentTimeMillis() / 1000L;
-        rpc.largeImageKey = "linuxmint";
+        rpc.largeImageKey = "melons";
         String largeText = DiscordText.get() + Config.get().version.getOriginalString();
-        if (!Config.get().devBuild.isEmpty()) largeText += " Dev Build: " + Config.get().devBuild;
-        rpc.largeImageText = largeText;
-        currentSmallImage = SmallImage.linux;
-        updateDetails();
+        if (!Config.get().devBuild.isEmpty()) {
+            largeText = largeText + "    : " + Config.get().devBuild;
+        }
 
+        rpc.largeImageText = largeText;
+        this.currentSmallImage = DiscordPresence.SmallImage.iro;
+        this.updateDetails();
         instance.Discord_UpdatePresence(rpc);
         instance.Discord_RunCallbacks();
     }
 
-    @Override
     public void onDeactivate() {
         instance.Discord_ClearPresence();
         instance.Discord_Shutdown();
     }
 
     @EventHandler
-    private void onTick(TickEvent.Post event) {
-        if (!Utils.canUpdate()) return;
-        ticks++;
+    private void onTick(Post event) {
+        if (Utils.canUpdate()) {
+            ++this.ticks;
+            if (this.ticks >= 200) {
+                this.currentSmallImage = this.currentSmallImage.next();
+                this.currentSmallImage.apply();
+                instance.Discord_UpdatePresence(rpc);
+                this.ticks = 0;
+            }
 
-        if (ticks >= 200) {
-            currentSmallImage = currentSmallImage.next();
-            currentSmallImage.apply();
-            instance.Discord_UpdatePresence(rpc);
-
-            ticks = 0;
+            this.updateDetails();
+            instance.Discord_RunCallbacks();
         }
-
-        updateDetails();
-        instance.Discord_RunCallbacks();
     }
 
     private void updateDetails() {
-        if (isActive() && Utils.canUpdate()) {
-            rpc.details = Placeholders.apply(line1.get());
-            rpc.state = Placeholders.apply(line2.get());
-
+        if (this.isActive() && Utils.canUpdate()) {
+            rpc.details = Placeholders.apply((String) this.line1.get());
+            rpc.state = Placeholders.apply((String) this.line2.get());
             instance.Discord_UpdatePresence(rpc);
         }
+
     }
 
-    private enum SmallImage {
-        linux("images", "iro"),
-        mint("images", "iro");
+    static {
+        instance = DiscordRPC.INSTANCE;
+    }
 
-        private final String key, text;
+    private static enum SmallImage {
+        iro("images", "iro"),
+        gramms("images", "gramms");
 
-        SmallImage(String key, String text) {
+        private final String key;
+        private final String text;
+
+        private SmallImage(String key, String text) {
             this.key = key;
             this.text = text;
         }
 
         void apply() {
-            rpc.smallImageKey = key;
-            rpc.smallImageText = text;
+            DiscordPresence.rpc.smallImageKey = this.key;
+            DiscordPresence.rpc.smallImageText = this.text;
         }
 
-        SmallImage next() {
-            if (this == linux) return mint;
-            return linux;
+        DiscordPresence.SmallImage next() {
+            return this == iro ? gramms : gramms;
         }
     }
 }
+
